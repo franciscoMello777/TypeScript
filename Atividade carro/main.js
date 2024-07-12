62974ec641826c91322e2b180981203e5233cbe9
franciscoMello777 <162368791+franciscoMello777@users.noreply.github.com>
2024-07-12 16:50:01 -0300

    Delete Atividade carro directory

--- Atividade carro/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Carro_1 = require("./Carro");
-var Carro_2 = require("./Carro");
-var meuCarro = new Carro_1.Carro("GTR R35", 20, "Nissan", 5);
-var minhaGarragem = new Carro_2.Garragem();
-minhaGarragem.addCarros(meuCarro);
--- Atividade carro/main.ts
+++
@@ -1,6 +0,0 @@
-import { Carro } from "./Carro";
-import { Garragem } from "./Carro";
-
-let meuCarro:Carro = new Carro("GTR R35", 20, "Nissan", 5)
-let minhaGarragem:Garragem = new Garragem()
-minhaGarragem.addCarros(meuCarro)
